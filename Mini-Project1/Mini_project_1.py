c3ef2a38c57cafbddd9023b0ed1c6763afd0cc3d
Felix <felixadrianlucaciu@gmail.com>
2018-10-11 16:24:43 -0400

    First commit for project1

---
+++ Mini-Project1/Mini_project_1.py
@@ -0,0 +1,26 @@
+# 
+#
+#
+#
+#   [	a,   b,   c,   d    ]
+# 	[	e,   f,   g,   h    ]
+# 	[	i,   j,   k,   l    ]
+# 
+#   There are 8 possible moves: 
+# 		- up
+# 		- Up-Right
+# 		- Right
+# 		- Down-Right
+# 		- Down
+# 		- Down-Left
+# 		- Left
+# 		- Up-Left
+#		
+
+
+
+
+input_string = input("Input the initial puzzle sequence")
+type(input_string)
+
+print("this is the initial puzzle sequence that was inputted: ", input_string)
